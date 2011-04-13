3eb25efec4e30a550a862e710351511026656083
Abel Muiño <amuino@gmail.com>
2011-04-13 13:21:14 -0700

    Updated instructions for installing passenger from apt

--- setup.sh
+++ setup.sh
@@ -20,10 +20,11 @@ rvm use 1.9.2 --default
 apt-get -y install mysql-client mysql-server
 
 # Phusion Pasenger
-apt-get -y install libcurl4-openssl-dev apache2-mpm-prefork apache2-prefork-dev libapr1-dev libaprutil1-dev
+apt-get -y install apache2-mpm-prefork libapache2-mod-passenger
 
-gem install passenger
+sh -c 'echo "<IfModule mod_passenger.c>\n  PassengerRoot /usr\n  PassengerRuby /usr/local/rvm/wrappers/ruby-1.9.2-p180/ruby\n</IfModule>" > /etc/apache2/mods-available/passenger.conf'
+sudo a2enmod passenger
 
-passenger-install-apache2-module
+/etc/init.d/apache2 restart
 
 echo "Worked" > /root/startup_script
