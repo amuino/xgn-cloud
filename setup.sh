f201ad56a144f4e7569f4d76d87323711155dc0c
Abel Muiño <amuino@gmail.com>
2011-04-13 12:14:04 -0700

    Added instructions as comments

--- setup.sh
+++ setup.sh
@@ -1,1 +1,5 @@
+# Run with the following line in the user-data parameter when launching an instance
+# #!/bin/sh 
+# curl https://github.com/amuino/xgn-cloud/raw/master/setup.sh | sh
+
 echo "Worked" > /root/startup_script
